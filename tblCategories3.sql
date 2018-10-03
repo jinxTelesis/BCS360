85132939bb65ef405ea616f864755a0be8de723a
Dre <31423756+jinxTelesis@users.noreply.github.com>
2018-10-02 20:36:47 -0400

    Create tblCategories3.sql

---
+++ tblCategories3.sql
@@ -0,0 +1,10 @@
+SET IDENTITY_INSERT tblCategories3 ON;
+INSERT INTO tblCategories3(CategoryID, CategoryName) VALUES
+(1,'guitar'),
+(2,'drums'),
+(3,'base'),
+(4,'sax'),
+(5,'electric Guitar');
+
+
+SET IDENTITY_INSERT tblCategories3 OFF;
